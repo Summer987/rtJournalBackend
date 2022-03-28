9647f265fc904a1b6865179f43c7c996a0715bc9
Dmitriy <d.efremov@sftpro.ru>
2022-03-28 15:59:48 +0300

    Added create user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {UserEntity} from "./user/entities/user.entity";
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      port: 5432,
+      username: 'user',
+      password: 'admin135',
+      database: 'tjournal',
+      host: 'localhost',
+      entities: [UserEntity],
+      synchronize: true,
+    }),
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
