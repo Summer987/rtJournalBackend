10ba5353b06ec3e344c31e19ff78997fb71c214a
Dmitriy <d.efremov@sftpro.ru>
2022-04-01 11:47:38 +0300

    Added auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import {UserService} from "../user/user.service";
+import {JwtService} from "@nestjs/jwt";
+import {UserEntity} from "../user/entities/user.entity";
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UserService) {}
+  constructor(
+    private usersService: UserService,
+    private jwtService: JwtService
+  ) {}
 
   async validateUser(email: string, password: string): Promise<any> {
     const user = await this.usersService.findByCond({
@@ -17,5 +22,14 @@ export class AuthService {
     }
 
     return null;
+  }
+
+  async login(user: UserEntity) {
+    const {password, ...userData} = user
+    const payload = { email: user.email, sub: user.id };
+    return {
+      ...userData,
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
