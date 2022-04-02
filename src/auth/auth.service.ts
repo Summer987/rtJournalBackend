8eeaf92b62df6cd2a8c4cdeba9d49715f9b95f2e
Dmitriy <d.efremov@sftpro.ru>
2022-04-02 12:33:51 +0300

    Added token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -42,7 +42,11 @@ export class AuthService {
 
   async register(dto: CreateUserDto) {
     try {
-      const { password, ...user } = await this.usersService.create(dto)
+      const { password, ...user } = await this.usersService.create({
+        email: dto.email,
+        fullName: dto.fullName,
+        password: dto.password
+      })
 
       return {
         ...user,
